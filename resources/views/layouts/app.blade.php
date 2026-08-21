d5b4b758f9d5b1821c9166a174c7866a3e47ec5a
COYT46 <thetrung017@gmail.com>
2026-08-21 16:42:02 +0700

    update fe

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,19 +37,19 @@
                 <a href="javascript:;"><i class="material-icons-outlined">menu</i></a>
             </div>
             <div class="search-bar flex-grow-1">
-                <h5 class="mb-0">Hệ thống Nhận diện Biển số xe</h5>
+                <h5 class="mb-0">Hệ thống nhận diện biển số xe</h5>
             </div>
             <ul class="navbar-nav gap-1 nav-right-links align-items-center">
                 <li class="nav-item dropdown">
                     <a href="javascript:;" class="dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
-                        <img src="{{ asset('public/admin/assets/images/avatars/01.png') }}"
-                            class="rounded-circle p-1 border" width="45" height="45">
+                        <img src="{{ asset('public/avatar/avatar.png') }}"
+                            class="rounded-circle p-1 border" width="45" height="45" alt="Avatar">
                     </a>
                     <div class="dropdown-menu dropdown-user dropdown-menu-end shadow">
                         <a class="dropdown-item  gap-2 py-2" href="javascript:;">
                             <div class="text-center">
                                 <h5 class="user-name mb-0 fw-bold">{{ auth()->user()->fullname ?? 'User' }}</h5>
-                                <p class="mb-0">{{ auth()->user()->role === 'manager' ? 'Quản lý' : 'Bảo vệ' }}</p>
+                                <p class="mb-0 text-muted">{{ auth()->user()->email }}</p>
                             </div>
                         </a>
                         <hr class="dropdown-divider">
